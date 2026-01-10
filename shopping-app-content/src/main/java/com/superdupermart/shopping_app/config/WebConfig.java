828a3038d3ebf3d42b02a73e074b579e015fed43
Jason Mui <jasonmui2004@gmail.com>
2026-01-10 11:17:06 -0500

    Changed WebConfig

--- shopping-app-content/src/main/java/com/superdupermart/shopping_app/config/WebConfig.java
+++ shopping-app-content/src/main/java/com/superdupermart/shopping_app/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","https://shoppingapp-frontend-g8e2.onrender.com" )
+                        .allowedOrigins("http://localhost:4200","https://shoppingapp-frontend-g8e2.onrender.com","https://shoppingfront.netlify.app" )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
